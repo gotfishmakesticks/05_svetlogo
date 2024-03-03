d0074837ca43e3e903a47e29a204225ddbe90baf
Фёдор Веселов <geometicalplayer@gmail.com>
2024-03-03 17:09:40 +0500

    Better player movement

--- Scripts/Entities/Animate/Player.cs
+++ Scripts/Entities/Animate/Player.cs
@@ -6,70 +6,79 @@ namespace _svetlogo.Entities.Animate
 	public partial class Player : CharacterBody2D
 	{
 		const float gravity = 98.1f;
-		const float jumpForce = 125f;
+		const float jumpForce = 50f;
 
 		[Export] private float max_speed;
 		[Export] private float acceleration_time;
 		[Export] private float deceleration_time;
 
+		private bool jump_cached;
 		private float acceleration;
 		private float deceleration;
+		private bool floor_cached;
+
+		private Timer jump_cache_timer;
 
 		public override void _Ready()
 		{
 			acceleration = max_speed / acceleration_time;
 			deceleration = max_speed / deceleration_time;
+
+			jump_cache_timer = GetNode<Timer>("JumpCacheTimer");
 		}
 
 		public override void _PhysicsProcess(double delta)
 		{
 			Vector2 velocity = Velocity;
 
+            float res_acceleration = 0.0f;
 
-			if (!IsOnFloor())
-				velocity.Y += gravity * (float)delta;
+			if (IsOnFloor() != floor_cached)
+			{
+				if (floor_cached)
+				{
+					jump_cache_timer.Start();
+				}
+				else
+				{
+					jump_cache_timer.Stop();
+					jump_cached = true;
+				}
+			}
 
-			if (Input.IsActionJustPressed("jump") && IsOnFloor())
-				velocity.Y = -jumpForce;
+            floor_cached = IsOnFloor();
+
+            if (IsOnFloor() || IsOnCeiling())
+			{
+				res_acceleration -= deceleration * velocity.X/max_speed;
+			}
 
 			float direction = Input.GetAxis("move_left", "move_right");
-			if (IsOnFloor())
+
+			if ((velocity.X < max_speed && direction > 0) || (velocity.X > -max_speed && direction < 0))
 			{
-				if (direction != 0 && velocity.X < max_speed)
-				{
-					if (Mathf.Sign(velocity.X) != direction)
-					{
-						velocity.X += direction * deceleration * (float)delta;
-					}
-					else
-					{
-						velocity.X += direction * acceleration * (float)delta;
-					}
-				}
-				else
-				{
-					velocity.X -= Mathf.Sign(velocity.X) * deceleration * (float)delta;
-				}
+				res_acceleration += direction * (acceleration + deceleration);
 			}
-			else
+			else if (direction == Mathf.Sign(velocity.X)) 
 			{
-				if (Mathf.Sign(velocity.X) != direction)
-				{
-					velocity.X += direction * deceleration * (float)delta * 0.33f;
-				}
-				else
-				{
-					velocity.X += direction * acceleration * (float)delta * 0.33f;
-				}
+                res_acceleration = 0;
 			}
 
-			if (Mathf.Abs(velocity.X) < 0.25)
+			velocity.X += res_acceleration * (float)delta;
+
+			velocity.Y += gravity * (float)delta;
+			if (Input.IsActionJustPressed("jump") && jump_cached)
 			{
-				velocity.X = 0;
+				velocity.Y = -jumpForce;
 			}
 
 			Velocity = velocity;
 			MoveAndSlide();
+        }
+
+		public void JumpCacheTimeout()
+		{
+			jump_cached = false;
 		}
-	}
+    }
 }
