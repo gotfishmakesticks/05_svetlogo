9ffd212c6cfd7cdf28866ef9cb640ec0f1c7ba3a
Фёдор Веселов <geometicalplayer@gmail.com>
2024-03-04 22:44:58 +0500

    Prop restructure

--- Scripts/Abilities/Inanimate/Sticky.cs
+++ Scripts/Abilities/Inanimate/Sticky.cs
@@ -8,22 +8,18 @@ namespace _svetlogo.Abilities
     {
         public override void OnReady(Entity invoker)
         {
-            throw new NotImplementedException();
         }
 
         public override void ProcessAbility(Entity invoker)
         {
-            throw new NotImplementedException();
         }
 
         public override void StartAbility(Entity invoker)
         {
-            throw new NotImplementedException();
         }
 
         public override void StopAbility(Entity invoker)
         {
-            throw new NotImplementedException();
         }
     }
 }
