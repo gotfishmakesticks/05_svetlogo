163c81b2e4724b672d37e5f0f6c7e23ccad5596f
Фёдор Веселов <geometicalplayer@gmail.com>
2024-03-10 14:04:46 +0500

    Stupidante

--- Scripts/Entities/Static/Portal/PortalCondition.cs
+++ Scripts/Entities/Static/Portal/PortalCondition.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace _svetlogo.Entities.Static;
 
+[GlobalClass]
 public abstract partial class PortalCondition : Resource
 {
     public abstract bool CheckCondition(SceneTree tree);
